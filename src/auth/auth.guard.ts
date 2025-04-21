c03036bfc6e14c307e0d08b22526199e42954246
Lela Fieta <lelafieta11@gmail.com>
2025-04-21 21:46:15 +0100

    APP Guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -3,9 +3,13 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
